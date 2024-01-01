3f92684bb37dcfbadc5ff401729ed34dd6b55d41
Michal Konečný <michal.konecny@websta.cz>
2024-01-01 09:27:50 +0100

    Add translations

--- app/javascript/int/cs/cs.js
+++ app/javascript/int/cs/cs.js
@@ -1,4 +1,4 @@
-import elementLocale from 'element-ui/lib/locale/lang/cs-CZ';
+import elementLocale from 'element-ui/lib/locale/lang/cs-CZ'
 
 const messages = {
   cs: {
@@ -6,35 +6,44 @@ const messages = {
       more: 'Další články z blogu →',
       published: 'Publikováno:',
       read: 'Přečíst →',
-      title: 'Aktuálně z blogu',
+      title: 'Aktuálně z blogu'
     },
     form: {
       invoiceForm: {
+        header: 'Základní údaje',
         fields: {
           invoiceKind: {
             label: 'Druh faktury',
-            inputHelp: 'Věnujte pozornost výběru druhu faktury. Pokud nejste plátci DPH, měli byste zvolit fakturu bez DPH!'
+            placeholder: 'Druh faktury',
+            inputHelp:
+              'Věnujte pozornost výběru druhu faktury. Pokud nejste plátci DPH, měli byste zvolit fakturu bez DPH!'
           },
           number: {
-            label: 'Číslo faktury',
+            label: 'Číslo faktury'
           },
           vs: {
-            label: 'Variabilní symbol',
+            label: 'Variabilní symbol'
           },
           ks: {
-            label: 'Konstantní symbol',
+            label: 'Konstantní symbol'
           },
           issedOn: {
             label: 'Datum vystavení',
+            placeholder: 'Datum vystavení'
           },
-          maturity: {
+          maturityKind: {
             label: 'Splatnost',
+            placeholder: 'Splatnost'
           },
+          validation: {
+            required: 'Povinné',
+            optional: 'Volitelné'
+          }
         }
-      },
+      }
     },
     ...elementLocale
-  },
-};
+  }
+}
 
-export default messages;
+export default messages
